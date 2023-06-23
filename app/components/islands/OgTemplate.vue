e20465fc7162e3de2820273e71a850a79fd07443
yuma-kitamura <jiyuujinunite@gmail.com>
2023-06-23 22:02:10 +0900

    test - update og-image

---
+++ app/components/islands/OgTemplate.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const props = defineProps({
+  title: String,
+})
+</script>
+
+<template>
+  <div
+    :style="{
+      display: 'flex',
+      fontSize: 40,
+      color: '#35495e',
+      background: '#fff',
+      width: '100%',
+      height: '100%',
+      textAlign: 'center',
+      justifyContent: 'center',
+      alignItems: 'center',
+      border: '24px solid #35495e',
+    }"
+  >
+    <div
+      :style="{
+        fontSize: 60,
+        fontStyle: 'normal',
+        letterSpacing: '-0.025em',
+        color: '#35495e',
+        marginTop: 30,
+        padding: '0 120px',
+        lineHeight: 1.4,
+        whiteSpace: 'pre-wrap',
+      }"
+    >
+      {{ title }}
+    </div>
+  </div>
+</template>
